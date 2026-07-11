9b9c7c5de2713e05afb75ecc19b72dbadcf4a6ea
GalacticQuasar <akashdfw@gmail.com>
2026-07-11 15:57:09 -0500

    fix: hero image fills view height

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const Hero = () => {
     return (
-        <section id="hero" className="w-full">
-            <img src="/images/hero.png" alt="HelloWorld Hero Image" className="mx-auto" />
+        <section id="hero" className="w-full h-screen">
+            <img src="/images/hero.png" alt="HelloWorld Hero Image" className="w-full h-full object-cover" />
         </section>
     );
 };
